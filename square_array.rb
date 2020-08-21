cbbf1e443930b9d233defab30420e68054de65e8
sam l <sluongo718@yahoo.com>
2020-08-21 23:40:15 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -9,13 +9,3 @@ def square_array(array)
   return answer
 end
 
-def square_array(array)
-  # your code here
-   answer = []   ```i set an empty array here
-  array.each do |num|
-       ``````try and find a way to push the squared num's into answer here
-   
-  end
-
-  answer   ```now i return the modified array
-end

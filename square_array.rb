efab847e81bfdb9daaaf01a470f0bf77cc28a54b
sam l <sluongo718@yahoo.com>
2020-08-21 23:40:06 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,21 @@
 def square_array(array)
   # your code here
+  answer = []
   array.each do |num|
-   answer = num * num
-   new_array =  []
-   new_array.push(answer)
-  end
+   new_num = num * num
+   answer.push(new_num)
   
+  end
+  return answer
 end
 
+def square_array(array)
+  # your code here
+   answer = []   ```i set an empty array here
+  array.each do |num|
+       ``````try and find a way to push the squared num's into answer here
+   
+  end
+
+  answer   ```now i return the modified array
+end
